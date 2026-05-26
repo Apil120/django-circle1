cb44f80e12efec2a15602b38b8c0b0ced3f05eb3
Apil120 <apiladhikari87@gmail.com>
2026-05-26 16:00:59 +0545

    login feature backend testing stuff

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home,read_blog
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("",home),
-    path("blog/<int:id>/",read_blog)
+    path("blog/<int:id>/",read_blog),
+    path('login/', auth_views.LoginView.as_view(template_name='templates/login.html'), name='login'),
 ]
